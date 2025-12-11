7a1c07416e319335f79b417efdb7a86606e53042
dang-nguyendd <s3759957@rmit.edu.vn>
2025-12-12 02:41:00 +0700

    add train without val

--- train.py
+++ train.py
@@ -208,7 +208,7 @@ if __name__ == '__main__':
     parser.add_argument('--clip', type=float, default=0.5, help='gradient clipping margin')
     parser.add_argument('--train_path', type=str, default='./data/train', help='path to train dataset')
     parser.add_argument('--train_save', type=str, default='MMS-Net+')
-    parser.add_argument('--resume_path', type=str, default='snapshots/MMS-Net+/last.pth', help='path to checkpoint for resume training')
+    parser.add_argument('--resume_path', type=str, default='', help='path to checkpoint for resume training')
     args = parser.parse_args()
 
     save_path = f'snapshots/{args.train_save}/'
