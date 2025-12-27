35289d9ac06341ce422a096fbffaa90fc3d1079f
dang-nguyendd <s3759957@rmit.edu.vn>
2025-12-28 01:57:11 +0700

    todo-sat

--- test.py
+++ test.py
@@ -126,7 +126,7 @@ def inference(model, args):
 # ---------------- Main ---------------------
 if __name__ == "__main__":
     parser = argparse.ArgumentParser()
-    parser.add_argument("--weight", type=str, default="./snapshots/MMS-Net+/best.pth")
+    parser.add_argument("--weight", type=str, default="./snapshots/MMS-Net+/last.pth")
     parser.add_argument("--test_path", type=str,
                         default="./data/test")
     args = parser.parse_args()
